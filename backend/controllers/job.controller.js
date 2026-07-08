db953ab8ddade48c9d2c98c1efd373d5a78b83e1
Muhammad Hassaan Qureshi <156497587+MHassaanQureshi@users.noreply.github.com>
2026-07-08 12:40:06 +0500

    Add files via upload

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -58,12 +58,84 @@ export const uploadJob = async (req, res) => {
 
 export const GetAllJobs = async (req,res) =>{
     try{
+        const keyword = req.query.keyword || "";
+        const query = {
+            $or:[
+                {title:{$regex:keyword, $options:"i"}},
+                {description:{$regex:keyword, $options:"i"}},
 
+            ]
+        }
+         const jobs = await Job.find(query);
+         if(!jobs){
+            return res.status(400).json({
+                message:"jo not found",
+                success:false,
+            })
+         }
+         return res.status(200).json({
+            message:"found job",
+            jobs,
+            success:true,
+         })
+
+        
     }catch(error){
         return res.status(401).json({
             message:`error:${error}`,
             success:false
         })
 
+    }
+}
+
+export const GetJobById = async(req,res)=>{
+    try{
+        const jobid = req.params.id;
+        const job = await Job.findById(jobid);
+        return res.status(200).json({
+            message:"job founded",
+            job,
+            success:true
+        })
+        if(!job){
+            return res.status(400).json({
+            message:"job not founded",
+            success:false
+        })
+        }
+
+    }
+    catch(error){
+        return res.status(401).json({
+            message:`got error:${error}`,
+            success:false,
+        })
+    }
+}
+export const GetAdminJob = async(req,res) =>{
+    try{
+
+        const userId = req.id;
+        const jobs = await Job.find({created_by:userId})
+        return res.status(200).json({
+            message:"found Jobs",
+            jobs,
+            success:true
+        })
+
+        if(!jobs){
+            return res.status(400).json({
+            message:"job not founded",
+            success:false
+        })
+        }
+
+    }
+    catch(error){
+        return res.status(401).json({
+            message:`caught an error : ${error}`,
+            success:false
+        })
     }
 }
--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -1,7 +1,10 @@
 import express from "express"
 import isAuthenticated from '../middleware/isAuthenticated.js';
-import { uploadJob } from "../controllers/job.controller.js";
+import { uploadJob,GetAllJobs,GetAdminJob,GetJobById } from "../controllers/job.controller.js";
 const JobRoutes = express.Router()
 JobRoutes.route("/uploadJob").post(isAuthenticated,uploadJob)
+JobRoutes.route("/Get").get(isAuthenticated,GetAllJobs)
+JobRoutes.route("/getJobByid").get(isAuthenticated,GetJobById)
+JobRoutes.route("/GetAdminJob").get(isAuthenticated,GetAdminJob)
 
 export default JobRoutes
